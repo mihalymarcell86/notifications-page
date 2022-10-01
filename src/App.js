0533ae2af25d981418f3597fa02775504349ff2e
Marcell Mihály <mihalymarcell86@gmail.com>
2022-10-01 19:46:25 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Attribution from "./components/Attribution";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Notifications 3 Mark all as read Mark Webber reacted to your recent post
+      My first tournament today! 1m ago Angela Gray followed you 5m ago Jacob
+      Thompson has joined your group Chess Club 1 day ago Rizky Hasanuddin sent
+      you a private message 5 days ago Hello, thanks for setting up the Chess
+      Club. I've been a member for a few weeks now and I'm already having lots
+      of fun and improving my game. Kimberly Smith commented on your picture 1
+      week ago Nathan Peterson reacted to your recent post 5 end-game strategies
+      to increase your win rate 2 weeks ago Anna Kim left the group Chess Club 2
+      weeks ago
+      <Attribution />
+    </>
   );
 }
 
